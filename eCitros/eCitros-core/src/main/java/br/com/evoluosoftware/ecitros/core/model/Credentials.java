5efb329645aa582adf9734c905200e319b35151d
Rodrigo Carlstrom <rodrigo.carlstrom@interfile.com.br>
2018-10-18 16:35:33 -0300

    Versão inicial

---
+++ eCitros/eCitros-core/src/main/java/br/com/evoluosoftware/ecitros/core/model/Credentials.java
@@ -0,0 +1,19 @@
+package br.com.evoluosoftware.ecitros.core.model;
+
+import java.io.Serializable;
+
+public class Credentials implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
---
+++ eCitros/eCitros-core/src/main/java/br/com/evoluosoftware/ecitros/core/model/Profile.java
@@ -0,0 +1,46 @@
+package br.com.evoluosoftware.ecitros.core.model;
+
+import java.io.Serializable;
+
+public class Profile implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String email;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
